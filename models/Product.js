7e1af7194617d03d11d18cc9cbc1e3fc50921fa1
tracy rose guajardo <tracyroseguajardo@gmial.com>
2022-08-04 16:10:14 -0700

    set up models

--- models/Product.js
+++ models/Product.js
@@ -10,6 +10,28 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_name: {
+      type: DataTypes.STRING
+    },
+    price: {
+      type: DataTypes.DECIMAL
+    },
+    stock: {
+      type: DataTypes.INTEGER
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "category",
+        key: "id"
+      }
+    },
   },
   {
     sequelize,
